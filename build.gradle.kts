e4548e450afc86104ba2b681f18f54e0aaadbd25
Dan Rusu <roosterdan@gmail.com>
2024-09-10 13:11:52 -0600

    Add version catalog and testing framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version libs.versions.kotlin
+    `jvm-test-suite`
 }
 
 group = "com.danrusu"
@@ -7,4 +8,18 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+plugins.withId("org.jetbrains.kotlin.jvm") {
+    dependencies {
+        testImplementation(libs.strikt)
+        testRuntimeOnly(libs.jUnitPlatformLauncher)
+    }
+    testing {
+        suites {
+            val test by getting(JvmTestSuite::class) {
+                useJUnitJupiter(libs.versions.jUnit)
+            }
+        }
+    }
 }
