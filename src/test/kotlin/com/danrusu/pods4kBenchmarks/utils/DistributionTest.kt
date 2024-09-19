54c3d6896976b8dc2dfe4be2b329cf836393246b
Dan Rusu <roosterdan@gmail.com>
2024-09-19 11:31:25 -0600

    Add the average value to Distribution

--- src/test/kotlin/com/danrusu/pods4kBenchmarks/utils/DistributionTest.kt
+++ src/test/kotlin/com/danrusu/pods4kBenchmarks/utils/DistributionTest.kt
@@ -1,6 +1,7 @@
 package com.danrusu.pods4kBenchmarks.utils
 
 import org.junit.jupiter.api.Test
+import strikt.api.expectThat
 import strikt.api.expectThrows
 import strikt.assertions.isEqualTo
 import strikt.assertions.message
@@ -11,6 +12,20 @@ class DistributionTest {
         expectThrows<IllegalArgumentException> {
             Distribution.Bounds(lowerBound = 10, upperBound = 9)
         }.message.isEqualTo("lowerBound (10) cannot be greater than the upperBound (9)")
+    }
+
+    @Test
+    fun `bounds computeAverage validation`() {
+        with(Distribution.Bounds(lowerBound = 0, upperBound = 10)) {
+            expectThat(computeAverageValue())
+                .isEqualTo(5.0)
+        }
+
+        // overflow is avoided
+        with(Distribution.Bounds(lowerBound = Int.MAX_VALUE - 8, upperBound = Int.MAX_VALUE)) {
+            expectThat(computeAverageValue())
+                .isEqualTo((Int.MAX_VALUE - 4).toDouble())
+        }
     }
 
     @Test
@@ -36,5 +51,28 @@ class DistributionTest {
                 21 to Distribution.Bounds(lowerBound = 101, upperBound = 1000),
             )
         }.message.isEqualTo("The percentages must add up to 100 (found 101)")
+    }
+
+    @Test
+    fun `average validation`() {
+        with(
+            Distribution(
+                80 to Distribution.Bounds(lowerBound = 10, upperBound = 10),
+                20 to Distribution.Bounds(lowerBound = 100, upperBound = 100),
+            )
+        ) {
+            expectThat(averageValue)
+                .isEqualTo(28.0) // 10 * 0.8 + 100 * 0.2
+        }
+
+        with(
+            Distribution(
+                80 to Distribution.Bounds(lowerBound = 0, upperBound = 10), // 5 average
+                20 to Distribution.Bounds(lowerBound = 100, upperBound = 120), // 110 average
+            )
+        ) {
+            expectThat(averageValue)
+                .isEqualTo(26.0) // 5 * 0.8 + 110 * 0.2
+        }
     }
 }
