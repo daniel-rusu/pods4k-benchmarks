1af9fbab017e55757cbbb805ce6eddb04822308c
Dan Rusu <roosterdan@gmail.com>
2026-07-28 13:46:03 -0600

    Extract FieldGeneratorFactory to standalone file

--- src/main/kotlin/com/danrusu/pods4kBenchmarks/utils/generators/fieldGenerator/FieldGenerator.kt
+++ src/main/kotlin/com/danrusu/pods4kBenchmarks/utils/generators/fieldGenerator/FieldGenerator.kt
@@ -1,8 +1,6 @@
 package com.danrusu.pods4kBenchmarks.utils.generators.fieldGenerator
 
-import com.danrusu.pods4kBenchmarks.utils.generators.GeneratorRngs
 import com.danrusu.pods4kBenchmarks.utils.generators.NullabilityPolicy
-import com.danrusu.pods4kBenchmarks.utils.generators.RandomNullabilityPolicy
 
 /**
  * Generates simple field values for benchmark setup.
@@ -67,31 +65,3 @@ abstract class FieldGenerator(
         }.nullable(generateValue)
     }
 }
-
-/**
- * Creates [FieldGenerator] instances from benchmark RNG streams.
- */
-fun interface FieldGeneratorFactory {
-    /** Creates a field generator using the provided RNG streams. */
-    fun create(generatorRngs: GeneratorRngs): FieldGenerator
-
-    companion object {
-        /** Creates a factory for non-null random simple fields. */
-        fun withRandomFields(): FieldGeneratorFactory = FieldGeneratorFactory { generatorRngs ->
-            RandomFieldGenerator(random = generatorRngs.dataGenerationRng)
-        }
-
-        /** Creates a factory whose nullable field methods generate null values [nullRatio] of the time. */
-        fun withRandomNullableFields(nullRatio: Double): FieldGeneratorFactory =
-            FieldGeneratorFactory { generatorRngs ->
-                RandomFieldGenerator(
-                    random = generatorRngs.dataGenerationRng,
-                    nullabilityPolicy = RandomNullabilityPolicy(
-                        nullRatio = nullRatio,
-                        random = generatorRngs.nullabilityDecisionsRng,
-                    ),
-                )
-            }
-    }
-}
-
